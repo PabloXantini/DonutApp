ea81e55edac3b77177e1107a1d32228f5b47d7e8
PabloXantini <santinypabloe130@gmail.com>
2026-02-10 09:52:52 -0600

    feat: TabVBarView added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,8 @@
+import 'package:donut_app/tab/burger_tab.dart';
+import 'package:donut_app/tab/donut_tab.dart';
+import 'package:donut_app/tab/pancake_tab.dart';
+import 'package:donut_app/tab/pizza_tab.dart';
+import 'package:donut_app/tab/smoothie_tab.dart';
 import 'package:donut_app/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +14,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late TabController _tabController;
   List<Widget> myTabs = [
     //donut tab
     const MyTab(iconName: 'Donut', iconPath: 'lib/icons/donut.png'),
@@ -58,8 +64,20 @@ class _HomePageState extends State<HomePage> {
             //2. Tabs (TabBar)
             TabBar(
               tabs: myTabs,
-            )
+            ),
             //3. Content (TabBarView)
+            Expanded(
+              child: TabBarView(
+                // controller: _tabController,
+                children: [
+                  DonutTab(),
+                  BurgerTab(),
+                  SmoothieTab(),
+                  PancakeTab(),
+                  PizzaTab()
+                ]
+              )
+            )
             //4. Cart
           ],
         ),    
